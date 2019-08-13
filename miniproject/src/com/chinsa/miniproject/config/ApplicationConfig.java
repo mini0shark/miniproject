94baa318db12d4c313c7083f3f413c91be4d0b03
Administrator <Administrator@117.17.143.71>
2019-08-13 15:18:03 +0900

    하라고

--- miniproject/src/com/chinsa/miniproject/config/ApplicationConfig.java
+++ miniproject/src/com/chinsa/miniproject/config/ApplicationConfig.java
@@ -7,6 +7,7 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
@@ -14,7 +15,7 @@ import com.mysql.jdbc.Driver;
 
 @Configuration
 @ComponentScan(basePackages= {"com.chinsa.miniproject.dao","com.chinsa.miniproject.service"})
-//@Import({MybatisConfig.class})
+@Import({MybatisConfig.class})
 public class ApplicationConfig {
 	
 	@Bean
