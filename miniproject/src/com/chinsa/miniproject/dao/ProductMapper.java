374bae5d4dbf5534f6350a0a57713973c2cf9853
Administrator <Administrator@117.17.143.73>
2019-08-13 16:25:14 +0900

    ProductMapper.java , ProductMapper.xml 바꿈

--- miniproject/src/com/chinsa/miniproject/dao/ProductMapper.java
+++ miniproject/src/com/chinsa/miniproject/dao/ProductMapper.java
@@ -1,8 +1,15 @@
 package com.chinsa.miniproject.dao;
 
+import java.util.List;
+
+import com.chinsa.miniproject.dto.ProductDTO;
 import com.chinsa.miniproject.mapper.Mapper;
 
 @Mapper
 public interface ProductMapper {
-	
+	public ProductDTO getProduct(int pNo);
+	public List<ProductDTO> getProducts();
+	public int updateProduct(ProductDTO product);
+	public int insertUser(ProductDTO product);
+	public int deleteUser(int pNo);
 }
