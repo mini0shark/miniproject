15693cd45f892ddf01c7bf79ad030332684964c9
Administrator <Administrator@117.17.143.71>
2019-08-20 13:37:43 +0900

    ck에디터 수정

--- miniproject/src/com/chinsa/miniproject/controllers/ProjectController.java
+++ miniproject/src/com/chinsa/miniproject/controllers/ProjectController.java
@@ -1,16 +1,22 @@
 package com.chinsa.miniproject.controllers;
 
+import java.io.File;
+import java.util.Date;
 import java.util.Map;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.chinsa.miniproject.dto.FileUploadDTO;
 import com.chinsa.miniproject.dto.UserDTO;
 
 @Controller
@@ -62,6 +68,46 @@ public class ProjectController {
 		System.out.println(map.get("soso"));
 		System.out.println(map.get("good"));
 		return "product/search";
+	}
+	
+	@SuppressWarnings("deprecation")
+	@PostMapping("/product/imageupload")
+	public String imageUpload(HttpServletRequest request, Model model, FileUploadDTO fileUpload) {
+		Date date = new Date();
+		int year = date.getYear();
+		int month = date.getMonth();
+		String monthStr = "";
+		if(month < 10) 
+			monthStr = "0"+month;
+		else 
+			monthStr = ""+month;
+		String defaultPath = request.getRealPath("/");
+		String contextPath = request.getSession().getServletContext().getContextPath();
+		String fileUploadPathTail = "ckImage/"+year+""+monthStr;
+		String fileUploadPath = defaultPath+"/"+fileUploadPathTail;
+		
+		try {
+			MultipartFile file = fileUpload.getUpload();
+			if(file!=null) {
+				String fileName = file.getOriginalFilename();
+				String fileNameExt = fileName.substring(fileName.indexOf(".")+1);
+				if(!"".equals(fileName)) {
+					File destD = new File(fileUploadPath);
+					if(!destD.exists()) {
+						destD.mkdirs();
+					}
+					File destination = File.createTempFile("ckeditor_", "."+fileNameExt, destD);
+					file.transferTo(destination);
+					
+					fileUpload.setFileName(destination.getName());
+					fileUpload.setPath(contextPath+"/"+fileUploadPathTail+"/"+destination.getName());
+					model.addAttribute("fileUpload", fileUpload);
+				}
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return "product/imageUpload";
 	}
 	
 	
